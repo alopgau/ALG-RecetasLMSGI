db75947043541d3db373c9b1ce5899eb6e0842a0
alopgau <alopezg@iesrafaelalberti.es>
2026-04-27 14:08:25 +0200

    recetas.json

--- transform.js
+++ transform.js
@@ -1,10 +1,12 @@
 const xml2js = require("xml2js")
 const fs = require("fs")
-const convertXML = async () => {
-    const xml = fs.readFileSync("recetas.xml", "utf-8")
+const xml = fs.readFileSync('recetas.xml', 'utf8');
 
-    const data = xml2js.parseStringPromise(xml)
-    JSON.stringify(data)
-    fs.writeFileSync("recetas.json",)
-}
-convertXML()
+xml2js.parseString(xml, (err, result) => {
+    if (err) {
+        console.error("Error al parsear el XML:", err);
+        return;
+    }
+    fs.writeFileSync("recetas.json", JSON.stringify(result,null,2))
+});
+
