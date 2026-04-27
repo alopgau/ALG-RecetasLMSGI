f2cdd4aa0b04067d7561772daaeb11aae2149f34
alopgau <alopezg@iesrafaelalberti.es>
2026-04-27 14:29:53 +0200

    tabla mas o menos

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const { it } = require("node:test");
+
 const loadJSON = () => {
     const data = await fetch("recetas.json")
     return data
 }
 
 const loadInTable = () => {
-    data.recetas.for
+    const table = document.querySelector("table")
+    data.recetas.receta.forEach(it => {
+         `<tr id="${it.$[0]}">${it.nombre}<tr>`
+        `<tr>${it.categoria[0]}<tr>`
+        `<tr>${it.tiempo[0]}<tr>`
+        `<tr>${it.dificultad[0]}<tr>`
+    });
 }
--- transform.js
+++ transform.js
@@ -1,5 +1,6 @@
 const xml2js = require("xml2js")
-const fs = require("fs")
+const fs = require("fs");
+const { isTypedArray } = require("util/types");
 const xml = fs.readFileSync('recetas.xml', 'utf8');
 
 xml2js.parseString(xml, (err, result) => {
@@ -8,5 +9,6 @@ xml2js.parseString(xml, (err, result) => {
         return;
     }
     fs.writeFileSync("recetas.json", JSON.stringify(result,null,2))
+    co
 });
 
